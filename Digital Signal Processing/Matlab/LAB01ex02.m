05e9a88e7b29160384b532122cb81920e26b2e85
Kaizen86 <danielbridgewater87@gmail.com>
2025-11-27 14:55:27 +0000

    Minor correction to DSP LAB01ex02

--- Digital Signal Processing/Matlab/LAB01ex02.m
+++ Digital Signal Processing/Matlab/LAB01ex02.m
@@ -20,7 +20,7 @@ plot(t, x);
 xlabel('time, s')
 ylabel('amplitude, arbitrary units')
 % Set axis limits (xmin,xmax, ymin,ymax)
-% xmax zooms in on the beginning 1/10th of the signal
+% xmax zooms in on the beginning 0.2s of the signal
 axis([0 1/fmin*20 -1 1])
 
 % Observations:
